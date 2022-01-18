ea2505ee6c88fdd49b14cb9fa86ff0a6542950c2
Farick-m <mfarhodm@gmail.com>
2022-01-18 16:43:19 +0000

    Finished clock (no extra features)

--- clock/clokc.cpp
+++ clock/clokc.cpp
@@ -1,1 +1,60 @@
-#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+    int hour, minute, second, a, error;
+    error = a = 0;
+
+
+        cout<<"Enter hour"<<endl;
+        cin>>hour;
+
+        cout<<"Enter minutes"<<endl;
+        cin>>minute;
+
+        cout<<"Enter seconds"<<endl;
+        cin>>second;
+
+        while(error == 0)
+        {
+        if(hour<24 && minute<60 && second<60)
+        {
+            error++;
+        }
+        else
+        {
+            system("clear");
+        }
+        }
+
+        while (a==0)
+        {
+            system("clear");
+            cout<<hour<<":"<<minute<<":"<<second<<endl;
+            sleep(1);           //delays the time to imitate increment of 1 second for each loop
+            second++;
+
+            if (second>59)
+            {
+                second=0;
+                minute++;
+            }
+            if (minute>59)
+            {
+                minute=0;
+                hour++;
+            }
+            if (hour>24)
+            {
+                hour=0;
+            }
+        }
+        return 0;
+}
+ 
+
+
+
